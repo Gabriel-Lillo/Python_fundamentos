ed5dd25ee82cbb2cfba401b6b4c562c76f35f70b
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2023-12-29 09:39:40 -0300

    Modificacion en contenido de introduccion y strings

---
+++ 4-Strings/secuenciaEscape.py
@@ -0,0 +1,11 @@
+
+""" La secuencia scape se utiliza con un slach invertido"""
+
+# Tipos:
+#\" -> Para conciderar comillas
+#\` -> Para conciderar comillas
+#\\ -> Se utiliza para conciderar el slash en el texo
+#\n -> hace un salto de linea en el texto
+
+curso = "Ultimate \"Python\"" # En este ejemplo podemos usar el back slach para poder conciderar las comillas repetidas
+print(curso)
