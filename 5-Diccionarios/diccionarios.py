bbbb75bee7c135a7af1431a444d2769fff4c43db
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2024-01-06 20:09:11 -0300

    Nuevo contenido

--- 5-Diccionarios/diccionarios.py
+++ 5-Diccionarios/diccionarios.py
@@ -1,5 +1,6 @@
 # Los diccionarioa son como las listas y tuplas con la salvedad que no se rigen con indices si no a una LLAVE!
 # pueden ser modificados en el trascurso del programa
+# Son iterables igual que las listas y tuplas
 
 # * los diccionarios son equivalente a los JSON
 
@@ -29,6 +30,11 @@ diccionario['usuario'] = 'eduardo_gpg'
 
 # Obtener valor mediante una llave
 print(diccionario['usuario']) 
+print(diccionario.get("usuario"))
+
+# Eliminar valor 
+del diccionario["descuento"]
+del (diccionario['descuento'])
 
 
 # Para obtener llaves y valores podemos utilizar los metodos .keys() .values()
@@ -38,6 +44,20 @@ diccionario.keys()
 diccionario.values()
 for key, value in diccionario.items(): #-> .items() para recorres sus valores
     print(key, value)
+
+
+
+# Ejemplo en una caso real
+usuarios =[
+    {"id": 1, "nombre": "Chanchito"},
+    {"id": 2, "nombre": "Gabriel"},
+    {"id": 3, "nombre": "Feliz"},
+    {"id": 4, "nombre": "Nicolas"}
+]    
+
+for usuario in usuarios:
+    nombre = usuario["nombre"]
+    print(nombre)
 
 
 # lOS DICCIONARIOS SON EQUIVALENTE A LOS JASON
--- 7-Funciones/lambda.py
+++ 7-Funciones/lambda.py
@@ -26,9 +26,7 @@ print(funcion_grados(10))
 
 '''
 Otros ejemplos:
-
+'''
 sin_parametros = lambda : True
 parametros_default = lambda p1=10, p2=20, p3=30 : p1 + p2 + p3
 con_asterisco = lambda *args, **kwargs : len(args) + len(kwargs)
-
-'''
