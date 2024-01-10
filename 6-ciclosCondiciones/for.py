a0edac83fa0b154f16a87bdfac369d24ec880e79
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2024-01-10 15:51:12 -0300

    nuevo contenido en ciclos

--- 6-ciclosCondiciones/for.py
+++ 6-ciclosCondiciones/for.py
@@ -1,7 +1,7 @@
 # Con el ciclo for podemos iterar por cada elementos de una coleccion de datos ( String, listas, tuplas, diccionarios)
 # Principalmente se utiliza para iterar una lista de elementos
 
-usuarios = [ 'usuario1','usuario2', 'usuario3', 'usuario4',] #-> las tuplas listas siempre se declaran en plural
+usuarios = [ 'usuario1','usuario2', 'usuario3', 'usuario4',] #-> las tuplas, listas siempre se declaran en plural
 
 for usuario in usuarios:
     print(usuario)
