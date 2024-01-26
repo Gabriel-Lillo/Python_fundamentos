57dcfc88950cd4d1d5f580929ac0ee4a7a0d684f
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2024-01-25 23:21:15 -0300

    Modulos

---
+++ 10-Modulos/1-Intro/app.py
@@ -0,0 +1,18 @@
+from usuario_impuesto import guardar, pagar_impuesto
+
+""" JAMAS usar * para importar todo de otro archivo.
+SIEMPRE especificar los elementos que queremos importar"""
+
+
+guardar()
+pagar_impuesto()
+
+
+
+"""
+NOTA:
+ La Carpeta __pycache__ se genera automaticamente y en ella 
+ almacena el cache del codigo compilado.
+
+ A esto se le llama modulos compilados.
+"""
---
+++ 10-Modulos/1-Intro/usuario_impuesto.py
@@ -0,0 +1,18 @@
+""" Modulos """
+# Los modulos apuntan a archivos 
+
+"""
+Trabajar con modulos es como erdenamos los archivos de un programa.
+para este ejemplo tenemos dos archivo: app.py y usuario_impuesto.py
+
+En usuario_impuesto.py declaramos cierta cantidad de codigo (es este caso dos funciones)
+para depues importarlos al archivo app.py
+
+"""
+
+def guardar():
+    print("Guardando")
+
+
+def pagar_impuesto():
+    print("Pagando impuesto")
